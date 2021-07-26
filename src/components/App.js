dcf59b2bfa730f30deb5c72f02ae56dd0a15bee1
Olga Smirnova <g.o.n.z.o.m.e@yandex.ru>
2021-07-26 17:17:54 +0300

    декларативный подход

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,28 @@ import ImagePopup from "./ImagePopup";
 import PopupWithForm from "./PopupWithForm";
 
 function App() {
+    const [isEditAvatarPopupOpen, setEditAvatarPopupOpen] = React.useState(false);
+    const [isEditProfilePopupOpen, setEditProfilePopupOpen] = React.useState(false);
+    const [isAddPlacePopupOpen, setAddPlacePopupOpen] = React.useState(false);
+
+    function handleEditAvatarClick(){
+        setEditAvatarPopupOpen(true);
+        const popupAvatar = document.querySelector('.popup_type_new-avatar');
+        popupAvatar.classList.add('popup_visible');
+    }
+
+    function handleEditProfileClick(){
+        setEditProfilePopupOpen(true);
+        const popupProfile = document.querySelector('.popup_type_edit-profile');
+        popupProfile.classList.add('popup_visible');
+    }
+
+    function handleAddPlaceClick(){
+        setAddPlacePopupOpen(true);
+        const popupCreate = document.querySelector('.popup_type_add-card');
+        popupCreate.classList.add('popup_visible');
+    }
+
     return (
         <div className="App">
             <body className="page">
@@ -13,13 +35,13 @@ function App() {
             <Main />
             <Footer />
             <>
-                <PopupWithForm name='new-avatar' title='Обновить аватар' value='Сохранить'>
+                <PopupWithForm name='new-avatar' title='Обновить аватар' value='Сохранить' onEditAvatar={handleEditAvatarClick}>
                     <div className="input input_type_new-avatar">
                         <input id="popup__input_avatar_link" required className="popup__input popup__input_avatar_link" type="url" name="avatar" placeholder="Ссылка на аватар"/>
                         <span id="popup__input_avatar_link--error"/>
                     </div>
                 </PopupWithForm>
-                <PopupWithForm name='edit-profile' title='Редактировать профиль' value='Сохранить'>
+                <PopupWithForm name='edit-profile' title='Редактировать профиль' value='Сохранить' onEditProfile={handleEditProfileClick}>
                     <div className="input input_type_edit-profile">
                         <input id="popup__input_text_name" required className="popup__input popup__input_text_name" type="text" name="name" value="Жак-Ив Кусто" placeholder="Имя"/>
                         <span id="popup__input_text_name--error"/>
@@ -27,7 +49,7 @@ function App() {
                         <span id="popup__input_text_job--error"/>
                     </div>
                 </PopupWithForm>
-                <PopupWithForm name='add-card' title='Новое место' value='Создать'>
+                <PopupWithForm name='add-card' title='Новое место' value='Создать' onAddPlace={handleAddPlaceClick}>
                     <div className="input input_type_add-card">
                         <input id="popup__input_place_name" required className="popup__input popup__input_place_name" type="text" name="name" placeholder="Название"/>
                         <span id="popup__input_place_name--error"/>
--- src/components/Main.js
+++ src/components/Main.js
@@ -3,44 +3,27 @@ import avatar from '../images/image.jpg';
 import buttonAdd from '../images/Addbutton.svg';
 import buttonEdit from '../images/Editbutton.svg';
 
-function Main() {
-    function handleEditAvatarClick(){
-        const popupAvatar = document.querySelector('.popup_type_new-avatar');
-        popupAvatar.classList.add('popup_visible');
-    }
-
-    function handleEditProfileClick(){
-        const popupProfile = document.querySelector('.popup_type_edit-profile');
-        popupProfile.classList.add('popup_visible');
-    }
-
-    function handleAddPlaceClick(){
-        const popupCreate = document.querySelector('.popup_type_add-card');
-        popupCreate.classList.add('popup_visible');
-    }
+function Main(props) {
 
     return (
         <div className="Main">
             <main className="content">
 
                 <section className="profile">
-                    <button className="profile__avatar-button" onClick={handleEditAvatarClick}/>
+                    <button className="profile__avatar-button" onClick={props.onEditAvatar}/>
                     <img src={avatar} className="profile__avatar" alt="Аватар"/>
                     <div className="profile__info">
                         <h1 className="profile__name">Жак ив кусто</h1>
-                        <button type="button" className="profile__edit-button" onClick={handleEditProfileClick}>
-                            <img src={buttonEdit} alt='Кнопка редактировани'/></button>
+                        <button type="button" className="profile__edit-button" onClick={props.onEditProfile}><img src={buttonEdit} alt='Кнопка редактировани'/></button>
                         <p className="profile__job">Исследователь океана</p>
                     </div>
-                    <button type="button" className="profile__add-button" onClick={handleAddPlaceClick}>
-                        <img src={buttonAdd} alt='Кнопка добавления'/></button>
+                    <button type="button" className="profile__add-button" onClick={props.onAddPlace}><img src={buttonAdd} alt='Кнопка добавления'/></button>
                 </section>
 
                 <div className="elements">
                     <template className="element">
                         <div className="element__container">
-                            <button aria-label="Trash" type="button" className="element__trash"/>
-                            <img className="element__img" alt='fgh'/>
+                            <button aria-label="Trash" type="button" className="element__trash"/><img className="element__img" alt='fgh'/>
                             <div className="element__group">
                                 <h2 className="element__text"/>
                                 <button aria-label="Like" type="button" className="element__like"/>

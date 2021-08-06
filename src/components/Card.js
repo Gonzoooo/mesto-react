a46b3d1596af970bd3ba774d5229066f1fb84db4
Olga Smirnova <9441797@gmail.com>
2021-08-06 19:44:33 +0300

    visability

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,16 @@
 import React from "react";
+import { CurrentUserContext } from "../contexts/CurrentUserContext";
 
 function Card(props) {
+    const currentUser = React.useContext(CurrentUserContext);
+
+    // Определяем, являемся ли мы владельцем текущей карточки
+    const isOwn = props.card.owner._id === currentUser._id;
+
+// Создаём переменную, которую после зададим в `className` для кнопки удаления
+    const cardDeleteButtonClassName = (
+        `${isOwn ? 'element__trash-visible' : 'element__trash'}`
+    );
 
     function handleClick() {
         props.onCardClick(props.card);
@@ -9,7 +19,7 @@ function Card(props) {
     return(
         <li className="element">
             <div className="element__container">
-                <button aria-label="Trash" type="button" className="element__trash"/>
+                <button aria-label="Trash" type="button" className={cardDeleteButtonClassName}/>
                 <img src={props.card.link} alt={props.card.name} className="element__img" onClick={handleClick}/>
                 <div className="element__group">
                     <h2 className="element__text">{props.card.name}</h2>
